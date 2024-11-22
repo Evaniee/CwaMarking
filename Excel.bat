d3d16152ad78e447137eb1bdf4bd6e98aff875cf
Evaniee <62286912+Evaniee@users.noreply.github.com>
2024-11-22 11:44:16 +0000

    Update Excel.bat
    Added os-sys to PIP install

--- Excel.bat
+++ Excel.bat
@@ -5,6 +5,7 @@ if not exist .venv (
     .venv\Scripts\pip3 install setuptools
     .venv\Scripts\pip3 install tk
     .venv\Scripts\pip3 install pywin32
+    .venv\Scripts\pip3 install os-sys
 ) else (
     echo Using existing venv. Delete .venv folder an re-run to re-create venv
 )
